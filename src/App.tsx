d9eed963a53cf5a235615aae02d376dd6c1f68fa
Rhys Pegrume <pegrume88@gmail.com>
2024-02-08 10:49:43 +0100

    set up auth and form files and the home and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import Home from './_root/pages/Home';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
 const App = () => {
   return (
     <main className='flex h-screen'>
       <Routes>
-        {/* public routes */}
-        <Route path='/sign-in' element={<SigninForm />}/>
+        <Route element={<AuthLayout />}>
+          {/* public routes */}
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+          
+        </Route>
         {/* private routes */}
         <Route index element={<Home />}/>
       </Routes>
